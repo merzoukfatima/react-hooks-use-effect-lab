c525647dd7fa5cb69124a12ea9ba0a24b52b6680
MerzoukFatima <merzoukfatimazahraa@gmail.com>
2023-09-26 01:28:48 +0100

    Assignment Completed

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
+  const decrementTime = () => {
+    if (timeRemaining > 0) {
+      setTimeRemaining(timeRemaining - 1);
+    } else {
+      // When timeRemaining hits 0, reset it to 10 seconds
+      setTimeRemaining(10);
+      onAnswered(false)
+    }
+  };
   // add useEffect code
+  useEffect(() => {
+    const timer = setInterval(() => {
+      decrementTime();
+    }, 1000);
+
+    return function cleanup() {
+      clearTimeout(timer);
+    };
+  }, [timeRemaining]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
@@ -13,7 +31,7 @@ function Question({ question, onAnswered }) {
   const { id, prompt, answers, correctIndex } = question;
 
   return (
-    <>
+    <div>
       <h1>Question {id}</h1>
       <h3>{prompt}</h3>
       {answers.map((answer, index) => {
@@ -25,7 +43,7 @@ function Question({ question, onAnswered }) {
         );
       })}
       <h5>{timeRemaining} seconds remaining</h5>
-    </>
+    </div>
   );
 }
 
